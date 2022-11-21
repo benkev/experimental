3147c9bdb70289528750415edc6f5e4d00fd2083
Leonid Benkevitch <lbenkev@gmail.com>
2022-11-21 10:54:39 -0500

    ...

--- revdig/rd32.c
+++ revdig/rd32.c
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/reverse-integer/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -7,28 +9,34 @@
 
 
 int main(int argc, char *argv[]) {
-    short x0 = 23767;  //atoi(argv[1]);
-    short x, y = 0, q, r, j;
-    short y_by_10, y_by_10_div_10, y_minus_r;
-    short oflw = 0;
+    int x0 = 23767;  //atoi(argv[1]);
+    int x, y = 0, q, r, j;
+    int y_by_10, y_by_10_div_10, y_minus_r;
+    int oflw = 0, sgn;
 
     if (argc > 0)
         x = x0 = atoi(argv[1]);
     else
         x = x0;
+
+    if (x < 0) {
+        sgn = -1;
+        x = -x;
+    }
+    
     
     j = 0;
-    while (x && j < 6) {
+    while (x && j < 16) {
         q = x / 10;
         r = x % 10;
 
         y_by_10 = 10*y;
         y_by_10_div_10 = y_by_10/10;
         
-        printf("%2d: y=%6hd, 10*y  = %6hd\n", j, y, 10*y);
+        printf("%2d: y=%6d, 10*y  = %6d\n", j, y, 10*y);
         
         if (y_by_10/10 != y) {
-            printf("Overflow! (10*y)/10 != y: %hd != %hd.\n",
+            printf("Overflow! (10*y)/10 != y: %d != %d.\n",
                    y_by_10/10, y);
             oflw = 1;
             break;
@@ -39,7 +47,7 @@ int main(int argc, char *argv[]) {
         y_minus_r = y - r;
 
         if (y_minus_r != y_by_10) {
-            printf("Overflow! y_minus_r != y_by_10: %hd != %hd.\n",
+            printf("Overflow! y_minus_r != y_by_10: %d != %d.\n",
                    y_minus_r, y_by_10);
             oflw = 1;
             break;
@@ -54,7 +62,10 @@ int main(int argc, char *argv[]) {
         printf("Reversal impossible!\n");
         return -1;
     }
-     
+
+    if (sgn < 0)
+        y = -y;
+    
     printf("%d ==> %d\n", x0, y); 
     
     return 0;
--- revdig/reverse_best.c
+++ revdig/reverse_best.c
@@ -1,3 +1,6 @@
+
+// https://leetcode.com/problems/reverse-integer/
+
 class Solution {
 public:
     int reverse(int x) {
