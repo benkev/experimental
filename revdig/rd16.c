8ef27a7116f2a74e4ebf49ddc8ccdb8b7efcfa84
Leonid Benkevitch <lbenkev@gmail.com>
2022-11-21 04:18:42 -0500

    revdig/rd16.c: Checking for integer overflow turned up very easy: if (10*y)/10 not equal y.

--- revdig/rd16.c
+++ revdig/rd16.c
@@ -7,30 +7,56 @@
 
 
 int main(int argc, char *argv[]) {
-    short x = 23767;  //atoi(argv[1]);
-    short y = 0, q, r, j;
-    short const sten = 10;
+    short x0 = 23767;  //atoi(argv[1]);
+    short x, y = 0, q, r, j;
+    short y_by_10, y_by_10_div_10, y_minus_r;
+    short oflw = 0;
 
-    j = 12;
-    while (x && j) {
+    if (argc > 0)
+        x = x0 = atoi(argv[1]);
+    else
+        x = x0;
+    
+    j = 0;
+    while (x && j < 6) {
         q = x / 10;
         r = x % 10;
 
-        //        if (y != 0 && ((sten*y)/y != sten)) {
-        if (sten*y < 0) {
-            printf("Overflow! 10*y  = %6hd > 32767.\n", sten*y);
+        y_by_10 = 10*y;
+        y_by_10_div_10 = y_by_10/10;
+        
+        printf("%2d: y=%6hd, 10*y  = %6hd\n", j, y, 10*y);
+        
+        if (y_by_10/10 != y) {
+            printf("Overflow! (10*y)/10 != y: %hd != %hd.\n",
+                   y_by_10/10, y);
+            oflw = 1;
             break;
         }
-            
-        y = sten*y + r;
-        printf("x=%6hd; q=%6hd; r=%6hd; y=%6hd, 10*y = %6hd, (10*y)/y=%6hd\n", \
-               x, q, r, y, sten*y, (sten*y)/y);
+
+        y = y_by_10 + r;
+
+        y_minus_r = y - r;
+
+        if (y_minus_r != y_by_10) {
+            printf("Overflow! y_minus_r != y_by_10: %hd != %hd.\n",
+                   y_minus_r, y_by_10);
+            oflw = 1;
+            break;
+        }
+        
+        
         x = q;
-        j = j - 1;
+        j = j + 1;
     }
 
-    printf("x=%6hd; q=%6hd; r=%6hd; y=%6hd\n", x, q, r, y);
-
+    if (oflw) {
+        printf("Reversal impossible!\n");
+        return -1;
+    }
+     
+    printf("%d ==> %d\n", x0, y); 
+    
     return 0;
 
 
