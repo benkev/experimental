44e74e3db600708044be69fd205782d4d7e393c6
Leonid Benkevitch <lbenkev@gmail.com>
2025-01-21 12:54:36 -0500

    Started build_money_change_tree.py.

--- money_change/build_money_change_tree.py
+++ money_change/build_money_change_tree.py
@@ -1,12 +1,20 @@
 help_txt = '''
-Exchange a sum of money with coins of the denominations given in
-the noms list. The noms list must be sorted in descending orderю
+tree = []
+build_money_change_tree(tree, money, noms)
 
-Example:
+Build a tree of all the possible exchanges a sum of money with coins
+of the denominations given in the noms list. The noms list must be
+sorted in descending order. The tree must be an empty list.
+
+Examples:
+
+  Exchange 13 cent for 10, 5, 3, 2, 1 cent coins:
+    tree = []
+    build_money_change_tree(tree, 13, [10, 5, 3, 2, 1])
+
   Exchange $1 for 1, 2, 5, 20, and 50 cent coins:
-
-    count = countChange(100, [50, 20, 10, 5, 2, 1])
-
+    tree = []
+    build_money_change_tree(tree, 100, [50, 20, 10, 5, 2, 1])
 
 '''
 
@@ -15,7 +23,7 @@ import copy
 
 ways = []
 
-def countChange(money, noms):
+def build_money_change_tree(money, noms):
 
     if len(noms) > 1:
         maxnom = noms.pop(0)
@@ -27,7 +35,7 @@ def countChange(money, noms):
         for i in range(niter,0,-1):      # Check all changes: niter..1
             leftover = money - i*maxnom
             noms1 = copy.copy(noms)
-            nways += countChange(leftover, noms1)
+            nways += build_money_change_tree(leftover, noms1)
 
             print('maxnom=%d, leftover=%d, nways=%d' % \
                   (maxnom, leftover, nways), ', noms=', noms)
@@ -43,7 +51,7 @@ if __name__ == '__main__':
     noms = [10, 5, 3, 2, 1]
     money = 23
 
-    count = countChange(money, noms)
+    count = build_money_change_tree(money, noms)
 
     print("count = ", count)
 
