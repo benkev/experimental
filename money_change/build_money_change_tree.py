dcd45de26a129a5882e67b93e0c4cf6ac2057ea1
Leonid Benkevitch <lbenkev@gmail.com>
2025-02-03 00:50:25 -0500

    ...

--- money_change/build_money_change_tree.py
+++ money_change/build_money_change_tree.py
@@ -9,29 +9,27 @@ sorted in descending order. The tree must be an empty list.
 Examples:
 
   Exchange 13 cent for 10, 5, 3, 2, 1 cent coins:
-    tree = []
-    build_money_change_tree(tree, 13, [10, 5, 3, 2, 1])
+    build_money_change_tree(13, [10, 5, 3, 2, 1])
 
   Exchange $1 for 1, 2, 5, 20, and 50 cent coins:
-    tree = []
-    build_money_change_tree(tree, 100, [50, 20, 10, 5, 2, 1])
+    build_money_change_tree(100, [50, 20, 10, 5, 2, 1])
 
 '''
 
-from pylab import *
+# from pylab import *
 import copy
 
 
 def build_money_change_tree(money, coins):
     
-    print("1. money = ", money, ", coins = ", coins)
+#    print("1. money = ", money, ", coins = ", coins)
 
     tree = []
 
     while True:
         coin = coins.pop(0)   # Extract the biggest coin from the list
         niter = money//coin
-        print("2. coin = ", coin, ", niter = ", niter)
+#        print("2. coin = ", coin, ", niter = ", niter)
 
         if len(coins) == 0:
             tree.append([coin for n in range(niter)])
@@ -41,7 +39,7 @@ def build_money_change_tree(money, coins):
             leftover = money - i*coin
             change = [coin for n in range(i)]
 
-            print("4. leftover = ", leftover, ", change = ", change)
+#            print("4. leftover = ", leftover, ", change = ", change)
 
             if leftover == coins[-1]:   # No need to dive into recursion
                 change.append(leftover)
@@ -50,15 +48,35 @@ def build_money_change_tree(money, coins):
 
                 subtree = build_money_change_tree(leftover, coins1)
 
-                print("subtree = ", subtree)
+#                print("subtree = ", subtree)
                 
                 change.extend(subtree)
 
             tree.append(change)
 
-            print("5. tree = ", tree)
+#            print("5. tree = ", tree)
 
     return tree
+
+
+#
+# exchanges_from_tree(tree, xcgs):
+#      Create a 1D-list xchg with all exchanges from tree.
+#
+
+def exchanges_from_tree(tree, xcgs):
+    
+    n_elem = len(tree)
+    for i in range(n_elem):
+        elem = tree[i]
+        if isinstance(elem, list):
+            exchanges_from_tree(elem, xcgs)
+            xcgs.extend(xcg)
+        else:
+            xcgs.append(elem)
+
+    return
+
 
 
 
@@ -92,22 +110,26 @@ if __name__ == '__main__':
 
     # print("\n==========================================\n")
 
-    money = 10
-    coins = [5, 3, 2, 1]
+    money = 20
+    coins = [10, 5, 3, 2, 1]
+    # money = 100
+    # coins = [50, 20, 10, 5, 2, 1]
 
     coins1 = copy.copy(coins)  # Backup  
     
     tree = build_money_change_tree(money, coins)
     
-    print("==========================================\n")
+#    print("==========================================\n")
 
     print("money = ", money, ", coins = ", coins1)
     print()
     print("tree = ")
     for i in range(len(tree)):
         print("  ", tree[i])
+        print()
 
 
+    xcgs = []
 
 
 
